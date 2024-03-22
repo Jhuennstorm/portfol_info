50026f01ad98aa6313f5ace254264de3aa4738ab
Jhuennstorm <143415703+Jhuennstorm@users.noreply.github.com>
2024-03-22 11:13:56 +0800

    Update script.js

--- public/frontend/js/script.js
+++ public/frontend/js/script.js
@@ -1,7 +1,7 @@
 /* ========================== typing animation ========================== */
 export function myFunction() {
     var typed = new Typed(".typing",{
-        strings:["","BSIT Student","Web Designer", "DRRM Responder", "web Developer", ],
+        strings:["","BSIT Student","Web Designer", "DRRM Responder", "Begginer Web Developer", ],
         typeSpeed:100,
         backSpeed:60,
         loop:true
